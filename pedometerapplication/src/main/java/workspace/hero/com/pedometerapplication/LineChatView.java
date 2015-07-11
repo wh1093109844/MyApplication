8c1f58257912dc443cde9249896e1c3be2952d21
wanghe <1093109844@qq.com>
2015-07-11 23:43:38 +0800

    update chatview

--- pedometerapplication/src/main/java/workspace/hero/com/pedometerapplication/LineChatView.java
+++ pedometerapplication/src/main/java/workspace/hero/com/pedometerapplication/LineChatView.java
@@ -6,10 +6,13 @@ import android.graphics.Color;
 import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.samsung.android.sdk.motion.SmotionPedometer;
@@ -38,6 +41,13 @@ public class LineChatView extends View {
     private List<Long> dateLine = new ArrayList<>();
     private Map<Long, SmotionPedometer.Info> infoMap = new HashMap<Long, SmotionPedometer.Info>();
 
+    private int buttomMargin = 100;
+    private int leftMargin = 50;
+    private int topMargin = 50;
+    private int rightMargin = 50;
+
+    private float mOffset = 0;
+
     public LineChatView(Context context) {
         super(context);
         initParam();
@@ -63,32 +73,45 @@ public class LineChatView extends View {
         }
         height = getHeight();
         width = getWidth();
-        drawDottedDine(canvas);
+        RectF rectF = getOffset(dateLine.get(dateLine.size() - 1));
+        if ((width - leftMargin - rightMargin) > rectF.left) {
+            mOffset = 0;
+        }
+        drawDottedDineY(canvas);
         drawCoordinate(canvas);
     }
 
     /**
-     * »­ÐéÏß×ø±ê
+     * ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½
      * @param canvas
      */
-    private void drawDottedDine(Canvas canvas) {
-        float itemHeight = (float)(height - 100) / 10;
-        mPaint.setColor(getResources().getColor(android.R.color.darker_gray));
-        canvas.drawLine(50f, 50f, 50f, (float) (height - 50), mPaint);
-
+    private void drawDottedDineY(Canvas canvas) {
+        float itemHeight = (float)(height - topMargin - buttomMargin) / 10;
+        mPaint.setColor(Color.BLACK);
+        mPaint.setPathEffect(null);
+        mPaint.setStrokeWidth(2);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawLine(leftMargin, topMargin, leftMargin, (float) (height - buttomMargin), mPaint);
+        Path path = new Path();
         for (int i = 0; i <= 10; i++) {
+
+            mPaint.setStrokeWidth(2);
+            mPaint.setStyle(Paint.Style.STROKE);
             if (i != 0) {
                 DashPathEffect effects = new DashPathEffect(new float[]{4, 4},1);
                 mPaint.setPathEffect(effects);
                 mPaint.setColor(getResources().getColor(android.R.color.darker_gray));
+            } else {
+                mPaint.setPathEffect(null);
+                mPaint.setColor(Color.BLACK);
             }
-            mPaint.setStrokeWidth(2);
-            mPaint.setStyle(Paint.Style.STROKE);
-            Path path = new Path();
-            float bottomHeight = (float)(height - 50) - itemHeight * i;
+
+
+            float bottomHeight = (float)(height - buttomMargin) - itemHeight * i;
             Log.d(TAG, "height:" + bottomHeight);
-            path.moveTo(50f, bottomHeight);
-            path.lineTo((float) (width - 50), bottomHeight);
+            path.reset();
+            path.moveTo(leftMargin, bottomHeight);
+            path.lineTo((float) (width - rightMargin), bottomHeight);
             canvas.drawPath(path, mPaint);
 
             mPaint.setStyle(Paint.Style.FILL);
@@ -99,8 +122,9 @@ public class LineChatView extends View {
             int num = i * 2;
             Rect rect = getTextBounds(String.valueOf(num), mPaint);
             Log.d(TAG, num + " : " + rect.width());
-            canvas.drawText(String.valueOf(num), 50 - rect.width() - 20, bottomHeight + rect.height() / 2, mPaint);
+            canvas.drawText(String.valueOf(num), leftMargin - rect.width() - 20, bottomHeight + rect.height() / 2, mPaint);
         }
+
         mPaint.setPathEffect(null);
     }
 
@@ -120,33 +144,50 @@ public class LineChatView extends View {
     }
 
     private void drawCoordinate(Canvas canvas) {
+        final int count = dateLine.size();
+        Path path = new Path();
+        RectF temp = new RectF();
+        temp.left = 0;
+        temp.top = 0;
+
+        RectF temp1 = null;
+        for (int i = 0; i < count; i++) {
+            long key = dateLine.get(i);
+            RectF currRect = getOffset(key);
+
+            if (currRect.left < mOffset) {
+                temp = currRect;
+            } else if (temp != null) {
+                temp = getBound(temp, currRect);
+                path.moveTo(leftMargin - mOffset + temp.left, (float) (height - buttomMargin) - temp.top);
+                temp = null;
+            }
+            if (currRect.left < width - rightMargin - leftMargin + mOffset) {
+                temp1 = currRect;
+                if (temp == null) {
+                    path.lineTo(leftMargin + currRect.left - mOffset, (float) (height - buttomMargin) - currRect.top);
+                    drawDottedDineX(canvas, currRect);
+                    drawText(canvas, currRect, key);
+                }
+            } else {
+                temp1 = getBound(temp1, currRect);
+                path.lineTo(leftMargin + temp1.right - mOffset, (float) (height - buttomMargin) - temp1.bottom);
+                break;
+            }
+        }
         mPaint.setColor(getResources().getColor(R.color.line_color));
+        mPaint.setPathEffect(null);
         mPaint.setStrokeWidth(2);
         mPaint.setStyle(Paint.Style.STROKE);
-        final int count = dateLine.size();
-
-        for (int i = 0; i < count; i++) {
-            Path path = new Path();
-            RectF startRect;
-            if (i != 0) {
-                startRect = getOffset(dateLine.get(i - 1));
-            } else {
-                startRect = new RectF();
-            }
-            path.moveTo(50 + startRect.left, (float) (height - 50) - startRect.top);
-
-            RectF currRect = getOffset(dateLine.get(i));
-            path.lineTo(50 + currRect.left, (float)(height - 50) - currRect.top);
-
-            canvas.drawPath(path, mPaint);
-        }
+        canvas.drawPath(path, mPaint);
     }
 
     private RectF getOffset(Long key) {
         SmotionPedometer.Info info = infoMap.get(key);
         RectF rectF = new RectF();
-        rectF.top = (float)info.getSpeed() * (height - 100) / 20;
-        rectF.left = ((key - startTime) * 50 ) / 1000;
+        rectF.top = (float)info.getSpeed() * (height - topMargin - buttomMargin) / 20;
+        rectF.left = (int)((key - startTime) * 50 ) / 1000;
+        rectF.left = rectF.left < 0 ? 0 : rectF.left;
         Log.d(TAG, "left:" + rectF.left + "      top:" + rectF.top + "    ti" + "         startTime:" + getDate(key));
         return rectF;
     }
@@ -157,4 +198,101 @@ public class LineChatView extends View {
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
         return sdf.format(calendar.getTime());
     }
+
+    private void drawDottedDineX(Canvas canvas, RectF currRect) {
+
+        DashPathEffect effects = new DashPathEffect(new float[]{4, 4},1);
+        mPaint.setPathEffect(effects);
+        mPaint.setColor(getResources().getColor(android.R.color.darker_gray));
+        mPaint.setStrokeWidth(2);
+        mPaint.setStyle(Paint.Style.STROKE);
+
+        Path path = new Path();
+
+        float x = leftMargin + currRect.left - mOffset;
+        float y1 = (float)(height - buttomMargin - 2);
+        float y2 = y1 - currRect.top;
+        path.moveTo(x, y1);
+        path.lineTo(x, y2);
+        canvas.drawPath(path, mPaint);
+        Log.d(TAG, "draw X,   lineTo:" + x + ", " + y2 + ", " + x + ", " + y1);
+    }
+
+    private void drawText(Canvas canvas, RectF currRect, long key) {
+        mPaint.setPathEffect(null);
+        mPaint.setColor(Color.BLACK);
+        mPaint.setStrokeWidth(1);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setTextSize(15);
+
+        Path path = new Path();
+        String str = getDate(key);
+        Rect rect = getTextBounds(str, mPaint);
+        float x = leftMargin + currRect.left - rect.height() / 2 - mOffset;
+        float y = (float)(height - buttomMargin + 2);
+
+        path.moveTo(x, y);
+        path.lineTo(x, height);
+        canvas.drawTextOnPath(str, path, 10, 0, mPaint);
+    }
+
+    private RectF getBound(RectF prevRect, RectF currRect) {
+        RectF rectF = new RectF();
+        float k = (currRect.top - prevRect.top) / (currRect.left - prevRect.left);
+        float b = currRect.top - k * currRect.left;
+        if (prevRect.left < mOffset) {
+            rectF.left = mOffset;
+            rectF.top = k * rectF.left + b;
+        } else {
+            rectF.left = prevRect.left;
+            rectF.top = prevRect.top;
+        }
+        if (currRect.left >= width - leftMargin - rightMargin + mOffset) {
+            rectF.right = width - leftMargin - rightMargin + mOffset;
+            rectF.bottom = k * rectF.right + b;
+        } else {
+            rectF.right = currRect.left;
+            rectF.bottom = currRect.top;
+        }
+
+        return rectF;
+    }
+
+    private float tempX = 0;
+    private float temp = 0;
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                tempX = event.getX();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                temp = event.getX() - tempX;
+                tempX = event.getX();
+                if ((temp <= 0 && isLast()) || (temp > 0 && mOffset > 0)) {
+                    mOffset -= temp;
+                    if (temp > 0 && mOffset < 0) {
+                        mOffset = 0;
+                    }
+                    RectF rectF = getOffset(dateLine.get(dateLine.size() - 1));
+                    if ((width - leftMargin - rightMargin + mOffset) > rectF.left) {
+                        mOffset = rectF.left - width + leftMargin + rightMargin;
+                    }
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return true;
+    }
+
+    private boolean isLast() {
+        RectF rectF = getOffset(dateLine.get(dateLine.size() - 1));
+        if ((width - leftMargin - rightMargin + mOffset) <= rectF.left) {
+            return true;
+        }
+        return false;
+    }
+
 }
